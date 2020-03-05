54f6639a5ae008cd291dae4973120e287d6a0f27
fengxianqin <fengxianqin01@chinatopcredit.com>
2020-03-05 17:43:19 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <Button :size="size" @click="handleClick">按钮</Button>
+  </div>
+</template>
+
+<script>
+import Button from './button'
+export default {
+  name: 'HelloWorld',
+  data(){
+    return {
+      size: 'lg'
+    }
+  },
+  props: {
+    msg: String
+  },
+  components: {
+    Button
+  },
+  beforeCreate(){
+    console.log('parent beforeCreate');
+  },
+  created(){
+    console.log('parent created');
+  },
+  beforeMount(){
+    console.log('parent beforeMount');
+  },
+  mounted(){
+    console.log('parent mounted');
+  },
+  beforeUpdate(){
+    console.log('parent beforeUpdate');
+  },
+  updated(){
+    console.log('parent updated');
+  },
+  beforeDestroy(){
+    console.log('parent beforeDestroy');
+  },
+  destroyed(){
+    console.log('parent destroyed');
+  },
+  methods: {
+    handleClick() {
+      this.size = 'sm';
+      console.log(this.size);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/button.vue
@@ -0,0 +1,68 @@
+<template>
+  <button
+    :type="type"
+    @click="clickHandle"
+    :class="{
+      [`btn-${size}`]: size,
+      [`btn-${loading}`]: loading
+    }"
+    :disabled="disabled || loading">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'btn',
+  props:{
+    disabled: Boolean,
+    loading: Boolean,
+    type: {
+      type: String,
+      default: 'button',
+      validator(value){
+        return (['button', 'submit', 'reset']).indexOf(value) > -1;
+      }
+    },
+    size: {
+      type: String,
+      validator(value){
+        return (['lg', 'sm']).indexOf(value) > -1;
+      }
+    }
+  },
+  beforeCreate(){
+    console.log('child beforeCreate');
+  },
+  created(){
+    console.log('child created');
+  },
+  beforeMount(){
+    console.log('child beforeMount');
+  },
+  mounted(){
+    console.log('child mounted');
+  },
+  beforeUpdate(){
+    console.log('child beforeUpdate');
+  },
+  updated(){
+    console.log('child updated');
+  },
+  beforeDestroy(){
+    console.log('child beforeDestroy');
+  },
+  destroyed(){
+    console.log('child destroyed');
+  },
+  methods: {
+    clickHandle(event) {
+      this.$emit('click', event);
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
